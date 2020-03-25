d0ea1fd835cac7947f5837d0b7e3f4c8d9cee64d
duanyytop <duanyytop@gmail.com>
2020-03-25 09:56:25 +0800

    feat: add min_fee_rate to tx_pool_info rpc

--- lib/src/core/type/tx_pool_info.dart
+++ lib/src/core/type/tx_pool_info.dart
@@ -6,15 +6,9 @@ class TxPoolInfo {
   String proposed;
   String totalTxCycles;
   String totalTxSize;
+  String minFeeRate;
 
-  TxPoolInfo(
-      {this.lastTxsUpdatedAt,
-      this.pending,
-      this.staging,
-      this.orphan,
-      this.proposed,
-      this.totalTxCycles,
-      this.totalTxSize});
+  TxPoolInfo({this.lastTxsUpdatedAt, this.pending, this.staging, this.orphan, this.proposed, this.totalTxCycles, this.totalTxSize, this.minFeeRate});
 
   factory TxPoolInfo.fromJson(Map<String, dynamic> json) {
     if (json == null) return null;
@@ -25,7 +19,8 @@ class TxPoolInfo {
         orphan: json['orphan'],
         proposed: json['proposed'],
         totalTxCycles: json['total_tx_cycles'],
-        totalTxSize: json['total_tx_size']);
+        totalTxSize: json['total_tx_size'],
+        minFeeRate: json['min_fee_rate']);
   }
 
   Map<String, dynamic> toJson() {
@@ -36,7 +31,8 @@ class TxPoolInfo {
       'orphan': orphan,
       'proposed': proposed,
       'total_tx_cycles': totalTxCycles,
-      'total_tx_size': totalTxSize
+      'total_tx_size': totalTxSize,
+      'min_fee_rate': minFeeRate
     };
   }
 }
